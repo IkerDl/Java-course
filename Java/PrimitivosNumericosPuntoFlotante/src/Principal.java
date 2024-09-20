6b5c3b624870c8b7bbe30474d4baffd06176ebc8
Iker Delgado <123637459+IkerDl@users.noreply.github.com>
2024-09-20 12:17:07 +0200

    Setters y Getters

--- Java/PrimitivosNumericosPuntoFlotante/src/Principal.java
+++ Java/PrimitivosNumericosPuntoFlotante/src/Principal.java
@@ -2,20 +2,13 @@ public class Principal {
 
     public static void main(String[] args) {
 
-        float miValorFloat = 2345.67F;//32 bits
-        double miValorDouble = 7.9876;//64 bits
-        //No tiene propiedades ni métodos porque es una variable primitiva
-        //miValorFloat.
+        float miValorFloat = 2345.67F; //32 bits
+        double miValorDouble = 7.9876; // 64bits
+        //No tiene propiedades ni metodos porque es una variable primitiva
 
         Float miObjetoFloat = 2345.67F;
-
-        //miObjetoFloat tiene propiedades y métodos porque es un objeto
         int resultado = miObjetoFloat.hashCode();
-        System.out.println("resultado = " + resultado);//1158847160
-
-
-
+        System.out.println("resultado = " + resultado);
     }
-
 
 }
